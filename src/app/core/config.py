aea590f5d766d925ede06c46d3b96bbd893cdf3f
phamnguyen1700 <169217913+phamnguyen1700@users.noreply.github.com>
2026-08-12 16:08:50 +0700

    [FIX] - CORS for public domain

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -59,7 +59,7 @@ class Settings(BaseSettings):
     order_currency: str = "AUD"
 
     cors_origins: str = (
-        "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173"
+        "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173,https://tonyshrimp.com.au"
     )
 
     @property
